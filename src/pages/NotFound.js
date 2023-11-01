212b146f24faa6f26e90276675b5088b52d3a2be
Alessandro Orlandelli <aleorla2909@gmail.com>
2023-11-01 20:29:33 +0100

    not found page button

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -12,6 +12,12 @@ function NotFound() {
                   404 <br />
                   <span className="text-4xl">Page Not Found</span>
                </h2>
+               <a
+                  className=" rounded-full bg-purple-400 py-2.5 px-5 mt-4 "
+                  href="/"
+               >
+                  Back to the home
+               </a>
             </div>
          </MainLayout>
       </div>
